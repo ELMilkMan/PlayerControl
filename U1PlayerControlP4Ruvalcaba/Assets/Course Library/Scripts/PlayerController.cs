4eaba70f830c61720ea767df5ed79138bbe2b88a
ELMilkMan <730254@student.dusd.net>
2025-03-10 12:02:16 -0700

    goppy smishmas
    on Lesson 1.3 - High Speed Chase
    part 2.Create a new script for the camera

--- U1PlayerControlP4Ruvalcaba/Assets/Course Library/Scripts/PlayerController.cs
+++ U1PlayerControlP4Ruvalcaba/Assets/Course Library/Scripts/PlayerController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed = 5.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +15,6 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         // we gonna make the truck go vroom
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
     }
 }
